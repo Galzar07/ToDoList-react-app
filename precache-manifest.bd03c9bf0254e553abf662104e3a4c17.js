03f34f4ca8dfc72667b5a6828d8f2135fe6f379e
Galzar07 <52508198+Galzar07@users.noreply.github.com>
2020-05-09 13:56:46 +0200

    Updates

---
+++ precache-manifest.bd03c9bf0254e553abf662104e3a4c17.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce314d61187ab4b8130e4b54f94aa00d",
+    "url": "/ToDoList-react-app/index.html"
+  },
+  {
+    "revision": "0a22fca118a68048dc29",
+    "url": "/ToDoList-react-app/static/css/main.7456d17b.chunk.css"
+  },
+  {
+    "revision": "cc07422b722f02a77acd",
+    "url": "/ToDoList-react-app/static/js/2.bf706c0b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDoList-react-app/static/js/2.bf706c0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a22fca118a68048dc29",
+    "url": "/ToDoList-react-app/static/js/main.a155a2b2.chunk.js"
+  },
+  {
+    "revision": "4ba7d2716218dc530399",
+    "url": "/ToDoList-react-app/static/js/runtime-main.b02456e6.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/ToDoList-react-app/static/media/header1.70e6670e.jpg"
+  }
+]);
